0d87e3c89e820a8e2566f8d48fe1c1ca8e3928cf
Brad <Bradrockk@gmail.com>
2024-02-26 07:16:18 -0500

    adjusted index line

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const Routes = require("./API")
 
 const friendsRoutes = require('./API/friends'); 
 const UserRoutes = require("./API/users")
 const ThoughtRoutes = require("./API/thoughts")
 
 // router end points for searches.
-router.use('/api', Routes);
 router.use('/api', friendsRoutes); 
 router.use('/api', UserRoutes); 
 router.use('/api', ThoughtRoutes)

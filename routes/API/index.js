267a4649f180d410be8a7b5459d5a1fdbb2906bd
Brad <Bradrockk@gmail.com>
2024-02-26 04:27:23 -0500

    added post routes for thoughts/users

--- routes/API/index.js
+++ routes/API/index.js
@@ -1,10 +1,12 @@
+const express = require("express")
 const router = require('express').Router();
-
+router.use(express.json());
 // import the models to read.
 const Thought = require("../../models/thought")
 const User = require("../../models/User")
 
 // get all thoughts.
+
 router.get('/thoughts', async (req, res) => {
     try {
       const thoughts = await Thought.find();
@@ -16,6 +18,7 @@ router.get('/thoughts', async (req, res) => {
   });
 
 // get all users.
+
 router.get('/users', async (req, res) => {
     try {
       const users = await User.find();
@@ -26,6 +29,42 @@ router.get('/users', async (req, res) => {
     }
   });
 
-// post new user
 // post new thought
+router.post('/thoughts', async (req, res) => {
+    try {
+      const { thoughtText, username } = req.body;
+      if (!thoughtText || !username) {
+        return res.status(400).json({ error: 'Must have a valid user and content.' });
+      }
+      const newThought = new Thought({
+        thoughtText,
+        username,
+      });
+      const savedThought = await newThought.save();
+      res.status(201).json(savedThought);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({error});
+    }
+  });
+
+// post new user
+router.post('/users', async (req, res) => {
+    try {
+      const { username, email } = req.body;
+      if (!username || !email) {
+        return res.status(400).json({ error: 'Username and email are required' });
+      }
+      const newUser = new User({
+        username,
+        email,
+      });
+      const createUser = await newUser.save();
+      res.status(201).json(createUser);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({error});
+    }
+  });
+  
 module.exports = router;

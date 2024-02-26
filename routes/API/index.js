d6c6c73c5e6cf570afdf2c4988624b672587507e
Brad <Bradrockk@gmail.com>
2024-02-26 04:52:49 -0500

    added delete routes for users/thoughts

--- routes/API/index.js
+++ routes/API/index.js
@@ -66,5 +66,37 @@ router.post('/users', async (req, res) => {
       res.status(500).json({error});
     }
   });
-  
+
+// delete thoughts
+  router.delete('/thoughts/:thoughtId', async (req, res) => {
+    try {
+      const { thoughtId } = req.params;
+      const deletedThought = await Thought.findByIdAndDelete(thoughtId);
+      if (!deletedThought) {
+        return res.status(404).json({error: 'Thought not found'});
+      }
+      res.status(200).json({ message: 'success!', deletedThought });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({error});
+    }
+  });
+
+// delete users
+router.delete('/users/:userId', async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const deletedUser = await User.findByIdAndDelete(userId);
+
+    if (!deletedUser) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    res.status(200).json({ message: 'User deleted!', deletedUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({error});
+  }
+});
+
 module.exports = router;
